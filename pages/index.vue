d88b4556edfb789fe3673d7b362eadb8b46295d6
pankokko <teshi41@gmail.com>
2020-09-19 22:00:13 +0900

    add[クイズ機能、首都]

--- pages/index.vue
+++ pages/index.vue
@@ -1,89 +1,106 @@
 <template>
-  <v-row justify="center" align="center">
+<v-row justify="center" align="center">
     <v-col cols="12" sm="8" md="6">
-      <div class="text-center">
-        <logo />
-        <vuetify-logo />
-      </div>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
+        <div class="text-center">
+            <div>
+            <v-alert v-if="alert.status === 'correct'" type="success">
+                {{ alert.alertMessage }}
+            </v-alert>
+                <v-alert v-else-if="alert.status === 'incorrect'" type="error">
+                {{ alert.alertMessage }}
+            </v-alert>
+            </div>
+         
+            <v-card class="mx-auto" max-width="344">
+                <v-card-title> COUNTRY QUIZ
+                </v-card-title>
+                <v-card-text>
+                    <div class="display-1 text--primary">
+                        {{ expectCountry.capital }} is capital of 
+                    </div>
+                    <div v-for='country in countries' :key="country.country_code" class="text--primary">
+                          <v-radio-group v-model="picked" :mandatory="false">
+                            <v-radio :label="country.name"  :value="country.name">
+                             {{ country.country_code }}
+                            </v-radio>
+                        </v-radio-group>
+                    </div>
+                </v-card-text>
+                    {{ picked !== false ? picked : 'your Answer is...' }}
+                <v-card-actions>
+                    <v-btn block color="primary" @click="postAnswer(picked)">
+                        Answer
+                    </v-btn>
+                </v-card-actions>
+            </v-card>
+        </div>
     </v-col>
-  </v-row>
+</v-row>
 </template>
 
 <script>
 import Logo from '~/components/Logo.vue'
 import VuetifyLogo from '~/components/VuetifyLogo.vue'
+import axios from 'axios'
 
 export default {
-  components: {
-    Logo,
-    VuetifyLogo
-  }
+    components: {
+        Logo,
+        VuetifyLogo
+    },
+    async asyncData(params) {
+            let response = await axios.get("https://restcountries.eu/rest/v2/all") 
+            const { data } = response
+            if (data) {
+                let countries = []
+                let expectCountry = {}
+                for (let index = 0; index < 4; index++) {
+                    const arrayIndex = Math.floor(Math.random() * 200);  
+                    countries.push({
+                        name: data[arrayIndex].name,
+                        capital: data[arrayIndex].capital,
+                        country_code: data[arrayIndex].numericCode,
+                    })
+                }
+                return {
+                    countries
+                }
+            }
+    },
+    data() {
+        return {
+            picked: false,
+            alert: {
+                status: '',
+                alertMessage: '',
+            },
+            expectCountry: {
+                name: 'default Country',
+                capital: 'default capital',
+                country_code: '0',
+                trueAnswer: false,
+            },
+        }
+    },
+      mounted(){
+        this.expectCountry = this.countries[Math.floor(Math.random() * this.countries.length)]
+        console.log(this.expectCountry)
+    },
+    methods: {
+        postAnswer(answer){
+            console.log(answer)
+            console.log(this.expectCountry)
+            if (answer === this.expectCountry.name) {
+                this.alert.status = "correct",
+                this.alert.alertMessage = 'correct！'
+                this.alert.type = 'success'
+
+            } else {
+                this.alert.status = "incorrect",
+                this.alert.alertMessage = 'incorrect'
+                this.alert.type = 'danger'
+            }
+        },
+    },
 }
 </script>
